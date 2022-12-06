c2a80466b3e0f6353d82cdb731da209b39af1fa6
Sean Yeon <74743859+cpasean@users.noreply.github.com>
2022-12-06 18:19:46 -0500

    Add files via upload

--- Project_Image Classifer_Flower/predict.py
+++ Project_Image Classifer_Flower/predict.py
@@ -9,23 +9,21 @@ import json
 parser = argparse.ArgumentParser(description='Image Classifiler_flower')
 
 parser.add_argument("--filepath", type=str, help='Filepath (./test_images/wild_pansy.jpg)')
+parser.add_argument("--class_names" , type=str, help='Class Names (label_map.json)')
 parser.add_argument("--model" , type=str, help='Model (my_model.h5)')
 parser.add_argument("--top_K", type=int, help='Classes per possibility to show from the top rank')
 
 arg = parser.parse_args()
-print(arg.filepath, arg.model, arg.top_K)
+print(arg.filepath, arg.class_names, arg.model, arg.top_K)
 
 # pass the arguments
 image = arg.filepath
+name = arg.class_names
 model = arg.model
 top_K = arg.top_K
 
 # model loading
 model = tf.keras.models.load_model(model, custom_objects={'KerasLayer': hub.KerasLayer}, compile=False)
-
-# get class names from json file
-with open('label_map.json', 'r') as f:
-    class_names = json.load(f)
 
 # define image size and batch
 image_size = 224
@@ -33,14 +31,17 @@ batch_size = 64
     
 # image file normalize 
 def process_image(image):
-    
+    """
+    input: original pixel image
+    output: process pixel image by the image_size    
+    """    
     image = tf.cast(image, tf.float32)
     image = tf.image.resize(image, (image_size, image_size))
     image /= 255
     return image.numpy()
 
 # predict and return possibilty & classes 
-def predict(image_path, model, top_K):
+def predict(image_path, name, model, top_K):
     im = Image.open(image_path)
     test_image = np.asarray(im)
     processed_test_image = process_image(test_image)
@@ -49,11 +50,17 @@ def predict(image_path, model, top_K):
     top_prob = probabilities[0][-top_K:][::-1].tolist()
     top_classes = prob_predict.argsort()[0][-top_K:][::-1]
     top_classes = [i for i in top_classes]
-    print(top_prob, top_classes)
+        
+    # get class names from json file
+    with open(name, 'r') as f:
+        class_names = json.load(f)
+    
+    class_name = [class_names[str(label + 1)] for label in top_classes]
+    print(top_prob, class_name)
 
     
 if __name__ == '__main__':
-    predict(image, model, top_K)
+    predict(image, name, model, top_K)
 
 
 
